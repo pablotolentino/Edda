f97e81be76c7c24c8fc72781e243466688932be7
Pablo Tolentino <pablotolentino@outlook.com>
2023-07-02 22:55:14 -0600

    avance tabs

---
+++ Edda.Recortes/Edda.UI/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Edda.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// TitleBar_MouseDown - Drag if single-click, resize if double-click
+        /// </summary>
+        //private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.ChangedButton == MouseButton.Left)
+        //        if (e.ClickCount == 2)
+        //        {
+        //            AdjustWindowSize();
+        //        }
+        //        else
+        //        {
+        //            Application.Current.MainWindow.DragMove();
+        //        }
+        //}
+
+        /// <summary>
+        /// CloseButton_Clicked
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// MaximizedButton_Clicked
+        /// </summary>
+        //private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    AdjustWindowSize();
+        //}
+
+        /// <summary>
+        /// Minimized Button_Clicked
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        //private void AdjustWindowSize()
+        //{
+        //    if (this.WindowState == WindowState.Maximized)
+        //    {
+        //        this.WindowState = WindowState.Normal;
+        //        MaxButton.Content = "1";
+        //    }
+        //    else
+        //    {
+        //        this.WindowState = WindowState.Maximized;
+        //        MaxButton.Content = "2";
+        //    }
+
+        //}
+    }
+}
